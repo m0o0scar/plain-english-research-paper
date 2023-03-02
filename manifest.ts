7c2d59513022ad47e5645cf529f7d1736e9d7df9
Oscar <moscartong@gmail.com>
2023-03-02 22:15:43 +0800

    chore: code formatting

--- manifest.ts
+++ manifest.ts
@@ -1,4 +1,4 @@
-import packageJson from "./package.json";
+import packageJson from './package.json';
 
 /**
  * After changing, please reload the extension at `chrome://extensions`
@@ -8,35 +8,30 @@ const manifest: chrome.runtime.ManifestV3 = {
   name: packageJson.name,
   version: packageJson.version,
   description: packageJson.description,
-  options_page: "src/pages/options/index.html",
-  background: { service_worker: "src/pages/background/index.js" },
+  options_page: 'src/pages/options/index.html',
   action: {
-    default_popup: "src/pages/popup/index.html",
-    default_icon: "icon-34.png",
-  },
-  chrome_url_overrides: {
-    newtab: "src/pages/newtab/index.html",
+    default_popup: 'src/pages/popup/index.html',
+    default_icon: 'icon-34.png',
   },
   icons: {
-    "128": "icon-128.png",
+    '128': 'icon-128.png',
   },
   content_scripts: [
     {
-      matches: ["http://*/*", "https://*/*", "<all_urls>"],
-      js: ["src/pages/content/index.js"],
-      css: ["assets/css/contentStyle.chunk.css"],
+      matches: ['https://paperswithcode.com/paper/*'],
+      js: ['src/pages/content/index.js'],
+      css: ['assets/css/contentStyle.chunk.css'],
     },
   ],
-  devtools_page: "src/pages/devtools/index.html",
   web_accessible_resources: [
     {
       resources: [
-        "assets/js/*.js",
-        "assets/css/*.css",
-        "icon-128.png",
-        "icon-34.png",
+        'assets/js/*.js',
+        'assets/css/*.css',
+        'icon-128.png',
+        'icon-34.png',
       ],
-      matches: ["*://*/*"],
+      matches: ['*://*/*'],
     },
   ],
 };
